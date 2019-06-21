6cff6cc6cd812f1dec435468b5339ec98a3e10f3
Y00R D00M <tomek.ziola@gmail.com>
2019-06-21 12:00:37 +0200

    "added domain object tests"

--- links/src/main/java/com/ziola/shortenurl/domain/LinkRequest.java
+++ links/src/main/java/com/ziola/shortenurl/domain/LinkRequest.java
@@ -1,6 +1,7 @@
 package com.ziola.shortenurl.domain;
 
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 
 import javax.persistence.*;
@@ -11,6 +12,7 @@ import java.util.Date;
 @Entity
 @Data
 @AllArgsConstructor
+@Builder
 @Table(name = "links")
 public class LinkRequest {
 
